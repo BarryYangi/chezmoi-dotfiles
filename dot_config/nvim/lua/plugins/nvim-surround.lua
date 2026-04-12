20123831012d5a9d27c5421a2dc00ed14363ed3f
BarryYangi <barry@yct.ee>
2026-04-12 11:11:51 +0800

    fix: migrate nvim-surround config to v4

--- dot_config/nvim/lua/plugins/nvim-surround.lua
+++ dot_config/nvim/lua/plugins/nvim-surround.lua
@@ -3,11 +3,24 @@ return {
   "kylechui/nvim-surround",
   version = "*", -- Use for stability; omit to use `main` branch for the latest features
   event = "VeryLazy",
-  opts = {
-    keymaps = {
-      insert = false,
-      insert_line = false,
-      visual = "gs",
+  init = function()
+    -- nvim-surround v4 no longer accepts keymaps in setup().
+    vim.g.nvim_surround_no_insert_mappings = true
+    vim.g.nvim_surround_no_visual_mappings = true
+  end,
+  keys = {
+    {
+      "gs",
+      "<Plug>(nvim-surround-visual)",
+      mode = "x",
+      desc = "Add surround around selection",
+    },
+    {
+      "gS",
+      "<Plug>(nvim-surround-visual-line)",
+      mode = "x",
+      desc = "Add surround around selection on new lines",
     },
   },
+  opts = {},
 }
